575443ebd755a5889f6932ef5950aeca5bda5ab2
BiriBiri2077 <75076912+BiriBiri2077@users.noreply.github.com>
2022-04-17 18:51:42 -0300

    Add files via upload

---
+++ biri.js
@@ -0,0 +1,44 @@
+
+
+let Biri = {
+    nome : "Josué",
+    idade : 17,
+    altura : 1.75, 
+}
+console.log(Biri);
+
+let Seven = {
+    nome: "Guilherme",
+    idade: 13,
+    altura: 1.65,
+}
+console.log(Seven);
+
+let Trasheyy = {
+    nome: "Cauã",
+    idade: 16,
+    altura: 1.82,
+}
+console.log(Trasheyy);
+
+let nome = Trasheyy;
+let idade = 17;
+let altura = 1.75;
+
+if (altura <= 17) {
+console.log('bom dia');
+}
+
+function resetaCor(){
+    corSite = " Braço ";
+}
+resetaCor();
+
+if (nome = 'BiriBiri');
+console.log(corSite);
+
+if(idade = 17);
+console.log('osu');
+
+if(nome = 'Trasheyy');
+console.log('sos');
