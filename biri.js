0c28942ca7afd186c18b31d217386d3e25cac088
BiriBiri2077 <75076912+BiriBiri2077@users.noreply.github.com>
2022-04-24 12:51:02 -0300

    Update biri.js

--- biri.js
+++ biri.js
@@ -57,5 +57,13 @@ function max(numero1,numero2) {
         return numero1;
     else return numero2;
 }
+//or
+let valorMaior = max(22,17);
+console.log(valorMaior);
+
+function max(numero1,numero2) {
+     return numero1>numero2 ? numero1: numero2 ;
+}
+
 
 
