b5cb838b522b87cca6bf889cc9c665be2a1356ef
BiriBiri2077 <75076912+BiriBiri2077@users.noreply.github.com>
2022-04-24 13:32:09 -0300

    Update biri.js

--- biri.js
+++ biri.js
@@ -99,3 +99,17 @@ function verificarVelocidade(velocidade) {
             console.log('Pontos',pontos);
     }
 }
+
+
+//review some values
+//function shows if each number Par or Impar
+
+exibirTipo(10);
+function exibirTipo(limite) {
+    for (let i = 0; i <= limite; i++) {
+        if ( i % 2 === 0)
+            console.log (i,'PAR');
+        else
+            console.log(i,'IMPAR');
+    }
+}
